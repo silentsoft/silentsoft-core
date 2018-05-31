fc0471f35fe93ef2715de2d8f84e2e0dc019c760
silentsoft <hyesung.lee@silentsoft.org>
2018-05-31 21:39:20 +0900

    removes platform dependent test cases

--- src/test/java/org/silentsoft/core/util/SystemUtilTest.java
+++ src/test/java/org/silentsoft/core/util/SystemUtilTest.java
@@ -1,8 +1,14 @@
 package org.silentsoft.core.util;
 
-import junit.framework.Assert;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.List;
 
+import org.apache.commons.io.IOUtils;
 import org.junit.Test;
+import org.silentsoft.core.util.SystemUtil.ProcessInfo;
+
+import junit.framework.Assert;
 
 public class SystemUtilTest {
 
@@ -21,7 +27,7 @@ public class SystemUtilTest {
 		Assert.assertEquals(true, SystemUtil.addLibraryPath("C:\\test"));
 	}
 	
-//	@Test
+	@Test
 	public void printOSArchitecture() {
 		System.out.println(SystemUtil.getOSArchitecture());
 	}
@@ -31,4 +37,40 @@ public class SystemUtilTest {
 		System.out.println(SystemUtil.getPlatformArchitecture());
 	}
 	
+	@Test
+	public void printOS() {
+		System.out.println(SystemUtil.getOS());
+	}
+	
+	@Test
+	public void printCurrentPID() {
+		System.out.println(SystemUtil.getCurrentProcessId());
+	}
+	
+	@Test
+	public void printCurrentProcess() {
+		long start = System.currentTimeMillis();
+		String pid = SystemUtil.getCurrentProcessId();
+		ProcessInfo processInfo = SystemUtil.getProcessInfoByPID(pid);
+		System.out.println(processInfo.getPid());
+		System.out.println(processInfo.getSessionId());
+		System.out.println(processInfo.getUserName());
+		System.out.println(processInfo.getImageName());
+		System.out.println(System.currentTimeMillis() - start + " ms");
+	}
+	
+//	@Test
+	public void printCurrentProcess2() throws Exception {
+		String psCommand = String.join("", "ps -eo ucomm=,pid=,sess=,user= ", "350");
+		String awkCommand = String.join("", "awk '{print ", "\"\\\"\"", String.join("\"\\\",\" \"\\\"\"", "$1", "$2", "$3", "$4"), "\"\\\"\"}'");
+		
+		Process p1 = Runtime.getRuntime().exec(psCommand);
+	    InputStream input = p1.getInputStream();
+	    Process p2 = Runtime.getRuntime().exec(awkCommand);
+	    OutputStream output = p2.getOutputStream();
+	    IOUtils.copy(input, output);
+	    output.close(); // signals grep to finish
+	    List<String> result = IOUtils.readLines(p2.getInputStream());
+	    System.out.println(result);
+	}
 }
