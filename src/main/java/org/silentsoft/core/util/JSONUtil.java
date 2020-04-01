00330166da35f03334cb03f824a8388f24db6b3f
silentsoft <hyesung.lee@silentsoft.org>
2020-04-01 21:19:09 +0900

    Add JSON string beautify option / Remove org.json dependency

--- src/main/java/org/silentsoft/core/util/JSONUtil.java
+++ src/main/java/org/silentsoft/core/util/JSONUtil.java
@@ -1,21 +1,38 @@
 package org.silentsoft.core.util;
 
-import org.json.JSONObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 
 public class JSONUtil {
 	
+	private static final Logger LOGGER = LoggerFactory.getLogger(JSONUtil.class);
+	
 	public static String ObjectToString(Object target) {
-		return new JSONObject(target).toString();
+		return ObjectToString(target, true);
 	}
 	
-	public static JSONObject ObjectToJSON(Object target) {
-		return new JSONObject(target);
+	public static String ObjectToString(Object target, boolean beautify) {
+		String value = null;
+		
+		try {
+			ObjectMapper objectMapper = new ObjectMapper();
+			objectMapper.configure(SerializationFeature.ORDER_MAP_ENTRIES_BY_KEYS, true);
+			objectMapper.configure(SerializationFeature.INDENT_OUTPUT, beautify);
+			
+			value = objectMapper.writeValueAsString(target);
+		} catch (JsonProcessingException e) {
+			LOGGER.error("", e);
+		}
+		
+		return value;
 	}
 	
 	public static <T> T JSONToObject(String target, Class<T> returnType) throws Exception {
-		return (T)new ObjectMapper().readValue(target, returnType);
+		return (T) new ObjectMapper().readValue(target, returnType);
 	}
 	
 }
